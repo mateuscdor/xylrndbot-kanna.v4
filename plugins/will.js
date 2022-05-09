f82226e28e916386d0465481db143854a7f1090d
kenzo021 <92974890+kenzo021@users.noreply.github.com>
2022-05-09 20:08:52 +0700

    Create will.js

---
+++ plugins/will.js
@@ -0,0 +1,51 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+let info = `Ada apa manggil Adminku 🐦🫵`
+
+let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+/*const message = {
+            document: { url: thumb },
+            jpegThumbnail: await (await fetch(thumb)).buffer(), fileName: global.wm, mimetype: td, fileLength: '9999999', pageCount: '999',
+            caption: info,
+            footer: wm,
+            templateButtons: [
+                {
+                    urlButton: {
+                        displayText: '🌎 OFFICIAL GROUP',
+                        url: sgc
+                    }
+                },
+                {
+                    callButton: {
+                        displayText: '📞 Add me',
+                        phoneNumber: nomorown
+                    }
+                },
+                {
+                    quickReplyButton: {
+                        displayText: 'MENU',
+                        id: '.menu'
+                    }
+                },
+                {
+                    quickReplyButton: {
+                        displayText: 'PING',
+                        id: '.ping'
+                    }
+                },
+                {
+                    quickReplyButton: {
+                        displayText: 'DONASI',
+                        id: '.donasi'
+                    }
+                },
+            ]
+        }
+        return await conn.sendMessage(m.chat, message)*/
+conn.reply(m.chat, info, m, { contextInfo: { externalAdReply: {title: global.wm, body: 'Will sedang tidak ada', sourceUrl: snh, thumbnail: fs.readFileSync('./thumbnail.jpg')  }}})
+}
+handler.customPrefix = /^(will|will|willll)$/i
+handler.command = new RegExp
+
+export default handler
